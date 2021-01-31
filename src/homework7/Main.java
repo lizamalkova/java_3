196eac48ebda2637abd9c395843306336cb42680
lizamalkova <liza_ml@mail.ru>
2021-01-31 13:09:29 +0100

    hw7

---
+++ src/homework7/Main.java
@@ -0,0 +1,11 @@
+package homework7;
+
+import java.lang.reflect.InvocationTargetException;
+
+import static homework7.TestManager.*;
+
+public class Main {
+    public static void main(String[] args) throws InvocationTargetException, IllegalAccessException {
+        start(Tests.class);
+    }
+}
